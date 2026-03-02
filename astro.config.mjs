9c1b7d4b4e38436346d2ca31f07bd9e5a0e353c7
krsdntr <1krisdiantoro@apps.ipb.ac.id>
2026-03-02 22:23:57 +0700

    fix: use server mode remove duplicate keystatic api route add prerender=true to slug pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import keystatic from '@keystatic/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   integrations: [react(), keystatic()],
 
   vite: {
