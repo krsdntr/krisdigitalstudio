a15cf3b17dff614e553a9fae4ed844c39c236722
krsdntr <1krisdiantoro@apps.ipb.ac.id>
2026-03-07 12:34:13 +0700

    fix(keystatic): remove SSR route collision for cloudflare build

--- src/pages/api/keystatic/[...params].js
+++
@@ -1,5 +0,0 @@
-import { makePage } from '@keystatic/astro/api';
-import keystaticConfig from '../../../../keystatic.config';
-
-export const all = makePage(keystaticConfig);
-export const prerender = false;
